102818d35aad445fa9d5673e95c91d232e68f18a
kimhsr <kimhsr@naver.com>
2022-01-22 02:43:36 +0900

    날씨api 사용

--- 1_날씨API.py
+++ 1_날씨API.py
@@ -1,1 +1,9 @@
-print("test")
+import json
+import requests
+
+api_key = "108dbe8bc77f920712ee49138e0fe752"
+url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format("Seoul", api_key)
+data = requests.get(url)
+result = json.loads(data.text)  # json -> 딕셔너리 자료형으로 변환
+print(result["name"])
+print(result["weather"][0]["main"])
