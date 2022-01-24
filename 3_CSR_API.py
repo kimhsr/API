5a4e08d33f52c1e1d9981f99262e1927a11e1922
kimhsr <kimhsr@naver.com>
2022-01-24 22:09:21 +0900

    CSR_API

---
+++ 3_CSR_API.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+client_id = "YOUR_CLIENT_ID" # 본인 아이디
+client_secret = "YOUR_CLIENT_SECRET" #본인 비밀번호
+lang = "Kor" # 언어 코드 ( Kor, Jpn, Eng, Chn )
+url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
+data = open('음성 파일 경로', 'rb') # 음성 파일 경로
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/octet-stream"
+}
+response = requests.post(url,  data=data, headers=headers)
+rescode = response.status_code
+if(rescode == 200):
+    print (response.text)
+else:
+    print("Error : " + response.text)
